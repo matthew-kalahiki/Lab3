e3e509ca9b31563d3bf6b0ecc1fa188e2507a795
Matthew Kalahiki <kalahikimw@hendrix.edu>
2021-02-21 22:25:53 -0600

    Auto stash before merge of "main" and "origin/main"

---
+++ Pop!/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMangager : MonoBehaviour
+{
+    public GameObject startButton;
+    public GameObject backgroundImage;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
