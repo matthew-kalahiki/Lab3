38f604c84b757251f1ab48fc79e8eb1b320708e4
Cameron Minor <minorcj@hendrix.edu>
2021-02-24 20:26:26 -0600

    Changed Starting Scene

--- Pop!/Assets/Scripts/GameManager.cs
+++ Pop!/Assets/Scripts/GameManager.cs
@@ -50,7 +50,7 @@ public class GameManager : MonoBehaviour
     public void StartButon()
     {
         startButton.SetActive(false);
-        StartCoroutine(LoadYourAsyncScene("CentralWorld", nextPlayerLoc));
+        StartCoroutine(LoadYourAsyncScene("LeftWorld", nextPlayerLoc));
         player.SetActive(true);
 
     }
