f9d13c64f3f00d5b0c09e61a40cf84ac5e03106f
Cameron Minor <minorcj@hendrix.edu>
2021-02-25 10:12:59 -0600

    Changes to RightWorld

--- Pop!/Assets/Scripts/GameManager.cs
+++ Pop!/Assets/Scripts/GameManager.cs
@@ -58,7 +58,7 @@ public class GameManager : MonoBehaviour
     public void StartButon()
     {
         startButton.SetActive(false);
-        StartCoroutine(LoadYourAsyncScene("LeftWorld", nextPlayerLoc));
+        StartCoroutine(LoadYourAsyncScene("CentralWorld", nextPlayerLoc));
         player.SetActive(true);
 
     }
